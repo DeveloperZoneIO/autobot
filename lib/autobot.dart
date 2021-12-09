70bb68a3b7ccf01defb6e4a2cdb79281fdaf7806
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-10 00:59:02 +0100

    [#1] Read config and print errors

--- lib/autobot.dart
+++ lib/autobot.dart
@@ -1,12 +1,20 @@
 import 'package:args/command_runner.dart';
 import 'package:autobot/commands/run/run.dart';
+import 'package:autobot/config_reader.dart';
 import 'package:autobot/package_info.dart';
+import 'package:dcli/dcli.dart';
 
 const kConfigFileName = 'autobot_config';
 
 void main(List<String> args) async {
   await PackageInfo.load();
-  CommandRunner(PackageInfo.name, PackageInfo.description)
-    ..addCommand(RunCommand())
-    ..run(args);
+
+  try {
+    final configYaml = ConfigReader.readConfig();
+    final runner = CommandRunner(PackageInfo.name, PackageInfo.description);
+    runner.addCommand(RunCommand(configYaml: configYaml));
+    runner.run(args);
+  } catch (e) {
+    print(red(e.toString()));
+  }
 }
