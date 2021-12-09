9ab80e0416a407f45c4d648cab2eda904915bc3b
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-09 21:17:09 +0100

    Create dart cli project

---
+++ bin/autobot.dart
@@ -0,0 +1,5 @@
+import 'package:autobot/autobot.dart' as autobot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${autobot.calculate()}!');
+}
---
+++ test/autobot_test.dart
@@ -0,0 +1,8 @@
+import 'package:autobot/autobot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
