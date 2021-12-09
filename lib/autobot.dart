aeb2580c2b7c92619af63b3a80e8997ca053d02f
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-10 00:57:01 +0100

    [#1] Add global const field with config file name

--- lib/autobot.dart
+++ lib/autobot.dart
@@ -2,6 +2,8 @@ import 'package:args/command_runner.dart';
 import 'package:autobot/commands/run/run.dart';
 import 'package:autobot/package_info.dart';
 
+const kConfigFileName = 'autobot_config';
+
 void main(List<String> args) async {
   await PackageInfo.load();
   CommandRunner(PackageInfo.name, PackageInfo.description)
