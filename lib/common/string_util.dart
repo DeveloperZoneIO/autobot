e97912ebef4e29baf85e12215eba02e81bad1708
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-10 01:40:40 +0100

    [#11] Fix stripMargin

--- lib/common/string_util.dart
+++ lib/common/string_util.dart
@@ -1,9 +1,17 @@
 extension StringUtil on String {
-  String stripMargin() {
-    return splitMapJoin(
-      RegExp(r'^', multiLine: true),
-      onMatch: (_) => '\n',
-      onNonMatch: (n) => n.trim(),
-    );
+  String stripMargin({String reduce = '\t'}) {
+    final lines = split('\n');
+    var minIndent = -1;
+
+    lines.forEach((line) {
+      final chars = line.split('');
+      final indentCount = chars.takeWhile((char) => char != reduce).length;
+      if (minIndent < 0) minIndent = indentCount;
+      if (indentCount < minIndent) minIndent = indentCount;
+    });
+
+    return lines.fold('', (value, line) {
+      return value + line.replaceRange(0, minIndent, '') + '\n';
+    });
   }
 }
