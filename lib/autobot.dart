869fcfdc22e3f11c4225875e616f161684d6761d
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-10 01:41:16 +0100

    [#11] Add init command

--- lib/autobot.dart
+++ lib/autobot.dart
@@ -1,4 +1,5 @@
 import 'package:args/command_runner.dart';
+import 'package:autobot/commands/init/init.dart';
 import 'package:autobot/commands/run/run.dart';
 import 'package:autobot/config_reader.dart';
 import 'package:autobot/package_info.dart';
@@ -13,6 +14,7 @@ void main(List<String> args) async {
     final configYaml = ConfigReader.readConfig();
     final runner = CommandRunner(PackageInfo.name, PackageInfo.description);
     runner.addCommand(RunCommand(configYaml: configYaml));
+    runner.addCommand(InitCommand());
     runner.run(args);
   } catch (e) {
     print(red(e.toString()));
