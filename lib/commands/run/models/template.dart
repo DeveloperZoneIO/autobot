e83f063e0201526555fe5e35f9e4af105fffa6b0
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-16 21:12:20 +0100

    Use dart_mappable for template parsing
    !

---
+++ lib/commands/run/models/template.dart
@@ -0,0 +1,29 @@
+import 'template.mapper.g.dart';
+
+class TemplateDef with Mappable {
+  TemplateDef({required this.inputs, required this.outputs});
+
+  final List<InputDef> inputs;
+  final List<OutputDef> outputs;
+}
+
+class InputDef with Mappable {
+  InputDef({required this.key, required this.prompt});
+
+  final String key;
+  final String prompt;
+}
+
+class OutputDef with Mappable {
+  OutputDef({
+    required this.content,
+    required this.path,
+    this.write = 'yes',
+    this.writeMethod,
+  });
+
+  final String content;
+  final String path;
+  final String write;
+  final String? writeMethod;
+}
