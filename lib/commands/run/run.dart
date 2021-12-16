0d2408c4251ac8f5cff919c7b194f3bdc1cc24ae
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-16 21:14:56 +0100

    Implement run command using new components

--- lib/commands/run/run.dart
+++ lib/commands/run/run.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:autobot/autobot.dart';
+import 'package:autobot/commands/run/models/template.dart';
+import 'package:autobot/commands/run/models/template.mapper.g.dart';
 import 'package:autobot/common/dcli_utils.dart';
 import 'package:autobot/common/exceptions.dart';
 import 'package:autobot/common/null_utils.dart';
 import 'package:autobot/common/yaml_utils.dart';
 import 'package:autobot/config_reader.dart';
+import 'package:dart_mappable/dart_mappable.dart';
 import 'package:dcli/dcli.dart';
 import 'package:mustache_template/mustache.dart';
 import 'package:yaml/yaml.dart';
@@ -14,17 +18,16 @@ import 'package:yaml/yaml.dart';
 part 'run_config_reader.dart';
 part 'environment_reader.dart';
 part 'input_reader.dart';
-part 'output_reader.dart';
 part 'output_writer.dart';
 part 'template_reader.dart';
 part 'utils/render_mixin.dart';
 part 'utils/string_to_bool.dart';
+part 'output_task_builder.dart';
 
 part 'models/config.dart';
 part 'models/input.dart';
 part 'models/template_input_definition.dart';
-part 'models/template_output_definition.dart';
-part 'models/write_task.dart';
+part 'models/output_task.dart';
 
 class RunCommand extends Command {
   static const kOptionTemplate = 'template';
@@ -52,34 +55,21 @@ class RunCommand extends Command {
     final userInputs = readInputs(template);
     final envFileInputs = readEnvironmentFile();
     final environmentInputs = readEnvironment();
-    final outputs = readOutputs(template);
-
-    writeOutputs(
-      outputs,
-      inputs: environmentInputs + envFileInputs + userInputs,
-    );
+    final inputs = environmentInputs + envFileInputs + userInputs;
+    final tasks = buildOuputTasks(template.outputs, inputs: inputs);
+    writeOutputs(tasks);
   }
 }
 
 extension FunctionalRunCommand on RunCommand {
-  YamlMap readTemplate() => RunTemplateReader(this).readTemplate();
-
-  List<Input> readInputs(YamlMap template) => InputReader(this) //
-      .collectInputDefinitionsFrom(template)
-      .askForInputvalues()
-      .getUserInputs();
-
-  List<TemplateOutputDefinition> readOutputs(YamlMap template) => OutputReader(this) //
-      .collectOutputsFrom(template)
-      .getOutputs();
-
-  void writeOutputs(List<TemplateOutputDefinition> outputs, {required List<Input> inputs}) => OutputWriter(this) //
-      .collectVariables(inputs)
-      .createWriteTasks(outputs)
-      .writeOutputs();
-
+  TemplateDef readTemplate() => RunTemplateReader(this).readTemplate();
+  List<Input> readInputs(TemplateDef template) => InputReader(this).askForInputvalues(template);
   List<Input> readEnvironment() => EnvironmentReader(this).readEnvironment();
   List<Input> readEnvironmentFile() => EnvironmentReader(this).readEnvironmentFiles();
-
   RunConfig readConfig() => RunConfigReader(this).readConfig();
+  List<OutputTask> buildOuputTasks(List<OutputDef> outputs, {required List<Input> inputs}) => OutputTaskBuilder(this) //
+      .collectInputs(inputs)
+      .buildTasks(outputs);
+
+  void writeOutputs(List<OutputTask> tasks) => OutputWriter(this).writeOutputs(tasks);
 }
