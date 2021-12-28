21a9b0c800873925869747c7abb0431c7cbb63b8
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-28 22:40:01 +0100

    [#14] Introduce resources

---
+++ lib/components/resources.dart
@@ -0,0 +1,8 @@
+import 'package:autobot/common/dcli_utils.dart';
+import 'package:dcli/dcli.dart';
+
+class Resources {
+  static const configFileName = 'autobot_config.yaml';
+  static String get pathLocalConfigFile => pwd + configFileName;
+  static String get pathGlobalConfigFile => homeDir + configFileName;
+}
