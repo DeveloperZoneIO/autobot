0453fbeddc3e004395e334791f9289c1805ff4e4
MIchael Pankraz <michipankraz@Michis-MacBook-Pro.local>
2021-12-30 16:39:30 +0100

    [#14] Enhance readYamlAs

--- lib/components/read_yaml_as.dart
+++ lib/components/read_yaml_as.dart
@@ -5,15 +5,20 @@ import 'package:autobot/common/exceptions.dart';
 import 'package:dcli/dcli.dart';
 import 'package:yaml/yaml.dart';
 
-R readYamlAs<R>(String filePath) {
+/// Reads the yaml file from [filePath] and returns the content as [YamlMap].
+YamlMap readYaml(String filePath) {
   try {
     final fileContent = read(filePath).toParagraph();
-    final fileContentAsYaml = loadYaml(fileContent);
-    final fileContentAsJson = jsonEncode(fileContentAsYaml);
-    return Mapper.fromJson<R>(fileContentAsJson);
+    return loadYaml(fileContent);
   } on ReadException catch (_) {
     throw TellUser((tell) => tell(red('$filePath doesn not exist')));
   } on YamlException catch (_) {
     throw TellUser((tell) => tell(red('$filePath is no valid yaml file')));
   }
+}
+
+/// Reads a yaml file from [filePath] and returns the content as a object of type [R].
+R readYamlAs<R>(String filePath) {
+  final fileContentAsJson = jsonEncode(readYaml(filePath));
+  return Mapper.fromJson<R>(fileContentAsJson);
 }
