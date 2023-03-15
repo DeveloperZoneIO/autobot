1a7e12ebb471727b1663fb8abd950d299bdb9115
Michael Pankraz <pankraz.mi@gmail.com>
2023-03-15 22:09:44 +0100

    Introduce provider

---
+++ lib/di/provider.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+typedef ProviderFactory<T> = T Function();
+
+typedef Disposer<T> = FutureOr Function(T param);
+
+abstract class Provider {
+  T get<T extends Object>();
+
+  void registerFactory<T extends Object>(
+    ProviderFactory<T> factory, {
+    String? instanceName,
+  });
+
+  void registerSingleton<T extends Object>(
+    T instance, {
+    String? instanceName,
+    Disposer<T>? dispose,
+  });
+
+  void registerLazySingleton<T extends Object>(
+    ProviderFactory<T> factory, {
+    String? instanceName,
+    Disposer<T>? dispose,
+  });
+
+  Future<void> clear();
+}
